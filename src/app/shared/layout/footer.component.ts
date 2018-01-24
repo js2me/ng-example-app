d3eeccd8c9e400d8e78434f065c7e5f18fc1c196
Volkov Sergei <truefunrun@hotmail.com>
2018-01-24 18:22:20 +0300

    fork me button, css fixes, changed footer work

--- src/app/shared/layout/footer.component.ts
+++ src/app/shared/layout/footer.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, HostBinding, NgZone} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'layout-footer',
@@ -6,6 +7,36 @@ import {Component} from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-  constructor() {
+  @HostBinding('class.show') showHideFooter: boolean = this.checkRouterUrlForShowFooter(this.router.url);
+
+  constructor(private router: Router, private ngZone: NgZone) {
+    console.log(this.router);
+    this.ngZone.runOutsideAngular(() => {
+      this.router.events.subscribe((routerEvent: any) => {
+        if (routerEvent instanceof NavigationEnd) {
+          console.log(routerEvent.url, 'express', (routerEvent.url === '/article' ||
+            routerEvent.url === '/account') && !this.showHideFooter);
+          if (this.checkRouterUrlForShowFooter(routerEvent.url) && !this.showHideFooter) {
+            this.ngZone.run(() => {
+              console.log('show');
+              this.showHideFooter = true;
+            });
+          }
+          else if (this.showHideFooter) {
+            this.ngZone.run(() => {
+              console.log('hide');
+              this.showHideFooter = false;
+            });
+          }
+        }
+      })
+    });
   }
+
+  checkRouterUrlForShowFooter(routerUrl: string): boolean {
+    return routerUrl === '/article' ||
+      routerUrl === '/account';
+  }
+
+
 }
